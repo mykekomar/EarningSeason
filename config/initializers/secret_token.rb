222d7cbd462a9b82835d94537e1718ab204907d5
Michael Komarnitsky <mykekomar@gmail.com>
2013-07-30 13:17:19 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EarningSeason::Application.config.secret_token = '5a1e3573680e647e3159bab57fca9a62869c6911e5034cff811ffad42ed34172c06424392d1d04a0f1a6fc241831760183de5e6d29bad3c5dbd0a080a612e087'
